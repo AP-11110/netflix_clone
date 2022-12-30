016cfbcb1a110300765d53a6e168b9fdd151653b
Art Pau <arturas.study@gmail.com>
2022-12-30 02:22:19 +0100

    movies & lists routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 const app = express();
 const PORT = process.env.PORT || 8800;
 
@@ -21,6 +23,8 @@ const connect = async () => {
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(PORT, () => {
     connect();
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -22,12 +22,69 @@ router.put("/:id", verifyToken, async (req, res) => {
     if(req.user.isAdmin) {
         try {
             const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-            res.status(201).json(savedMovie);
+            res.status(200).json(updatedMovie);
         } catch (err) {
             res.status(500).json(err);
         }
         
     } else res.status(403).json("Not authorized!");
+});
+
+// update
+router.delete("/:id", verifyToken, async (req, res) => {
+    if(req.user.isAdmin) {
+        try {
+            await Movie.findByIdAndDelete(req.params.id);
+            res.status(200).json("Movie deleted");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+        
+    } else res.status(403).json("Not authorized!");
+});
+
+// get
+router.get("/find/:id", async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.status(200).json(movie);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get random with optional ?type=series
+router.get("/random", async (req, res) => {
+    const type = req.query.type;
+    let movie;
+    try {
+        if(type === "series") {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: true }},
+                { $sample: { size: 1 }}
+            ])
+        } else {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: false }},
+                { $sample: { size: 1 }}
+            ])
+        }
+        res.status(200).json(movie);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get all movies
+router.get("/", verifyToken, async (req, res) => {
+    if(req.user.isAdmin) {
+        try {
+            const movies = await Movie.find();
+            res.status(200).json(movies.reverse()); // most recent 1st
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else res.status(403).json("Not authorized!");   
 });
 
 module.exports = router;
